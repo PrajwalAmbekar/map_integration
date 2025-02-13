7bae89967cc649c7011c7b96d22500f90cdff4bc
Prajwal <ambekarprajwal33@gmail.com>
2025-02-13 23:10:38 +0530

    pin model created

---
+++ backend/src/models/pin.model.js
@@ -0,0 +1,34 @@
+import mongoose from"mongoose";
+
+const PinSchema= mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+        min:3,
+    },
+    desc:{
+        type:String,
+        required:true,
+        min:3
+    },
+    rating:{
+        type:Number,
+        required:true,
+        min:0,
+        max:5,
+    },
+    lat:{
+        type:Number,
+        require:true,
+    },
+    long:{
+        type:Number,
+        require:true,
+    },
+    timestamps:true
+});
+export const pin=mongoose.model("pins",PinSchema);
